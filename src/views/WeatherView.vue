f67a3b894369950238e41772549a7bcc29d5019c
ycao0070 <yccao0070@student.monash.edu>
2024-10-18 15:51:01 +1100

    Change requests from http to https.

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -62,7 +62,7 @@ export default {
       //Get the current weather icon using the API link
       iconUrl() {
         return this.weatherData
-          ? `http://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+          ? `https://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
           : null;
       },
     },
@@ -82,7 +82,7 @@ export default {
           navigator.geolocation.getCurrentPosition(async (position) => {
             const { latitude, longitude } = position.coords;
             //API link to obtain the current weather based on the current location browser identified 
-            const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
             //await means wait for the fetchWeatherData method to complete before proceeding
             await this.fetchWeatherData(url);
           });
@@ -93,7 +93,7 @@ export default {
             alert("Please enter a city name!");
             return;
         }
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`;
         await this.fetchWeatherData(url);
       },
       async fetchWeatherData(url) {
